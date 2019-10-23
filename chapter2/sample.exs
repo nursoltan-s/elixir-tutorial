371c72f050abb1199072543bc9cb61e7ecae153a
Nursoltan Saipolda <nursoltan.s@gmail.com>
2019-10-24 07:40:28 +0800

    chapter2

---
+++ chapter2/sample.exs
@@ -0,0 +1,21 @@
+# Basic Arithmetics
+
+1 + 2
+5 * 5
+10 / 2
+
+# h round/1 prints documentation of round method
+# function identified by its function name and arity. Arity is number of arguments that function can take.
+# For example: h round/1 -> round can take 1 argument.
+
+# Atoms
+
+# Atoms are like symbols in Ruby.
+
+:apple == :apple
+
+# String 
+
+# strings can hold utf-8 characters
+
+ 
